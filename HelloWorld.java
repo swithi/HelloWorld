2ad37cd5ea725d111ca72f0a0a1a360f2dafb5fd
Swithi <swithi@gmail.com>
2020-07-14 10:52:03 +0530

    Modified HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,9 @@ public class HelloWorld {
 
 	{
 
-		System.out.println("\n Hello World ");
+	      for(int i=0; i<10; i++) 
+                System.out.println("\n Hello World ");
+                
 
 	}
 
